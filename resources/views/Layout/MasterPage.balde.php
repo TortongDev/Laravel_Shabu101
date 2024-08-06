6f3f478321fe8014e69d7e4edcbf2b8e888e1a1c
TortongDev <kittithat.charoenkhet101@gmail.com>
2024-08-06 16:40:48 +0700

    tortong added file master page in views

---
+++ resources/views/Layout/MasterPage.balde.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="header-title">
+        Master Data
+    </div>
+</header>
+<nav>
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Food Menu</a></li>
+        <li><a href="">Food Type</a></li>
+        <li><a href="">Food Discount</a></li>
+        <li><a href="">Help</a></li>
+
+    </ul>
+</nav>
