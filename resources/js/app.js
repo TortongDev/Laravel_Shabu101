54acbe9164c22a3026e0c0aa725a1ed79b301d0b
TortongDev <kittithat.charoenkhet101@gmail.com>
2024-08-06 15:38:31 +0700

    added bootstrap and jquery in laravel and modified router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
+import  $  from 'jquery';
 import './bootstrap';
+
+$(document).ready(()=>{
+    console.log('test jquery...');
+
+})
---
+++ resources/views/view.blade.php
@@ -0,0 +1,6 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+<div class="card">
+    <div class="card-header">Hello World.</div>
+    <div class="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, molestias.</div>
+    <div class="card-footer">Lorem, ipsum.</div>
+</div>
